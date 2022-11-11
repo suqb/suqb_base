b4e981d033e08633f3e5c0edadeb63a7b283fe83
suqb <82658167+suqb@users.noreply.github.com>
2022-11-11 14:23:51 +0800

    添加方法

--- Hello.java
+++ Hello.java
@@ -3,5 +3,10 @@ package com.suqb;
 public class Hello{
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
+		method();
+		
+	}
+	public static void method() {
+		System.out.println("嘿嘿嘿");
 	}
 }
