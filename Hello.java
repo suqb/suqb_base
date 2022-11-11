67ce5e9e0fd223a7b69bb2cc647ef48af921ddc7
DW1OR <108138790+DW1OR@users.noreply.github.com>
2022-11-11 15:13:47 +0800

    78lao

--- Hello.java
+++ Hello.java
@@ -4,9 +4,10 @@ public class Hello{
 	public static void main(String[] args) {
 		System.out.println("Hello Git");
 		method();
-		
+		System.out.println("7tou");
 	}
 	public static void method() {
 		System.out.println("嘿嘿嘿");
 	}
+
 }
