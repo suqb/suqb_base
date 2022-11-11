d24150fd7253b24dc820cb6859becadec0d3b2d5
suqb1 <2907829008@qq.com>
2022-11-11 21:06:15 +0800

    测试

--- src/main/java/com/xxx/springboot/controller/Controller.java
+++ src/main/java/com/xxx/springboot/controller/Controller.java
@@ -3,6 +3,6 @@ package com.xxx.springboot.controller;
 public class Controller {
 
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello");
     }
 }
